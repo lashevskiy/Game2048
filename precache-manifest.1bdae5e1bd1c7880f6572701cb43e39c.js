c4c0706f19c5e0283861828d4faf711614e3ade2
Artem Lashevskiy <lashevskiy@gmail.com>
2019-05-11 23:26:00 +0300

    Updates

---
+++ precache-manifest.1bdae5e1bd1c7880f6572701cb43e39c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/Game2048/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "86a394a3128288ca7178",
+    "url": "/Game2048/static/js/runtime~main.f72e5985.js"
+  },
+  {
+    "revision": "20f5ae79f211df85c409",
+    "url": "/Game2048/static/js/main.31a13a45.chunk.js"
+  },
+  {
+    "revision": "221beba9f20da561b388",
+    "url": "/Game2048/static/js/2.fa21c9a7.chunk.js"
+  },
+  {
+    "revision": "20f5ae79f211df85c409",
+    "url": "/Game2048/static/css/main.4ad3a1d6.chunk.css"
+  },
+  {
+    "revision": "221beba9f20da561b388",
+    "url": "/Game2048/static/css/2.f2910be6.chunk.css"
+  },
+  {
+    "revision": "70909836c333a29ff87792efbb36a21e",
+    "url": "/Game2048/index.html"
+  }
+];
